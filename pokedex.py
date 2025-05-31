80c33fa30dc1db0448d22a67e2365e11b6e7d1e3
ReneSE73 <renesoes@gmail.com>
2025-05-31 15:44:23 -0600

    Release2

--- pokedex.py
+++ pokedex.py
@@ -1,81 +1,92 @@
-#importacion de librerias
+# Este código obtiene información de un Pokémon desde la API de PokeAPI y muestra sus detalles, incluyendo una imagen, peso, tamaño, movimientos, habilidades y tipos.
+# Requiere las librerías requests, pandas, matplotlib y PIL para funcionar correctamente.
+# Asegúrate de tenerlas instaladas en tu entorno de Python.
+# Puedes instalar las librerías necesarias con: pip install requests pandas matplotlib pillow
 
-import requests  # Para hacer solicitudes HTTP
-import pandas as pd # Para manejar datos en forma de DataFrame
-import matplotlib.pyplot as plt # Para crear gráficos y visualizaciones
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox # Para manejar imágenes en los gráficos
-import matplotlib.patches as patches # Para manejar parches en los gráficos
-from PIL import Image # Para manejar imágenes
-from io import BytesIO # Para manejar datos en memoria como si fueran archivos
-
-
-
-# Solicitud al usuario para ingresar el nombre del Pokémon
-pokemon = input("Ingresa el nombre del Pokémon: ").lower()
-url = "https://pokeapi.co/api/v2/pokemon/" + pokemon
+import requests # Importa la librería requests para realizar solicitudes HTTP
+import json # Importa la librería json para manejar datos en formato JSON
+import pandas as pd # Importa pandas para manejar datos en forma de DataFrame
+import matplotlib.pyplot as plt # Importa matplotlib para crear gráficos y visualizaciones
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox # Importa OffsetImage y AnnotationBbox para manejar imágenes en los gráficos
+import matplotlib.patches as patches # Importa patches para crear formas en los gráficos
+from PIL import Image # Importa Image de PIL para manejar imágenes
+from io import BytesIO # Importa BytesIO para manejar datos binarios en memoria
 
 
-# Realizar la solicitud a la API de Pokémon y verificar el estado de la respuesta
-respuesta = requests.get(url)
-if respuesta.status_code != 200:
-    print("Ha ocurrido un error. Intenta nuevamente")
-    print(f"Error: {respuesta.status_code}")
-    exit()
 
-# Procesar la respuesta JSON para extraer información del Pokémon
-datos = respuesta.json()
-
-# Verificar si el Pokémon existe
-try:
-    url_imagen = datos['sprites']['other']['official-artwork']['front_default']
-
-    # Verificar si la URL de la imagen es válida
-    if not url_imagen:
-        url_imagen = datos['sprites']['front_default']
+def obten_pokemon(pokemon):
+    """
+    Obtiene información de un Pokémon desde la API de PokeAPI asignando el nombre o clave de un pokemon muestra sus detalles.
     
-    respuesta_imagen = requests.get(url_imagen)
+    """
+    url = "https://pokeapi.co/api/v2/pokemon/" + pokemon
+    respuesta = requests.get(url)
     
-    # Obtener la imagen del Pokémon y Caracteriticas
-    imagen = Image.open(BytesIO(respuesta_imagen.content))
-    peso = datos['weight']
-    tamano = datos['height']
-    movimientos = [movimiento['move']['name'] for movimiento in datos['moves'][:10]] 
-    habilidades = [habilidad['ability']['name'] for habilidad in datos['abilities']]
-    tipos = [tipo['type']['name'] for tipo in datos['types']]
+    if respuesta.status_code != 200:
+        print("Ha ocurrido un error. Intenta nuevamente")
+        print(f"Error: {respuesta.status_code}")
+        return None
 
-# Manejo de excepciones para errores al obtener la imagen
-except:
-     print("No se pudo obtener la imagen del Pokémon.")    
-     exit()
+    datos = respuesta.json()
 
-# Crear una lista con la información del Pokémon
-pokemonlist = [datos['name'].capitalize(), url_imagen ,peso, tamano, movimientos, habilidades, tipos]
+    mostrar_informacion(datos)
 
-# Crear un objeto Series de pandas con la información del Pokémon
-s = pd.Series(pokemonlist, index=['Nombre', 'URLImagen', 'Peso', 'Tamaño', 'Movimientos', 'Habilidades', 'Tipos'])
+def mostrar_informacion(datos):
+    """Muestra la información del Pokémon obtenido, incluyendo su imagen, peso, tamaño, movimientos, habilidades y tipos."""
+    try:
+        url_imagen = datos['sprites']['other']['official-artwork']['front_default'] # obtiene la URL de la imagen del Pokémon
+        if not url_imagen:
+            url_imagen = datos['sprites']['front_default'] # obtiene la URL de la imagen del Pokémon si no hay otra disponible
+        
+        respuesta_imagen = requests.get(url_imagen)
+        imagen = Image.open(BytesIO(respuesta_imagen.content))
+        
+        peso = datos['weight'] # obtiene el peso del Pokémon
+        tamano = datos['height']  # obtiene el tamaño del Pokémon
+        movimientos = [movimiento['move']['name'] for movimiento in datos['moves'][:15]]    # obtiene los primeros 15 movimientos del Pokémon 
+        habilidades = [habilidad['ability']['name'] for habilidad in datos['abilities']] # obtiene las habilidades del Pokémon
+        tipos = [tipo['type']['name'] for tipo in datos['types']] # obtiene los tipos del Pokémon
 
-# Guardar la información del Pokémon en un archivo JSON
-df= pd.DataFrame([s]).to_json('pokedex/pokemon.json', index=False)
+    except Exception as e:
+         print(f"No se pudo obtener la imagen del Pokémon. Error: {e}")    # Manejo de excepciones para errores al obtener la imagen
+         return None
 
-# Dibujar la imagen del Pokémon y mostrar la información en un gráfico
-fg, ax = plt.subplots(figsize=(7, 5))
-imagenbox = OffsetImage(imagen, zoom=0.5)
-ab = AnnotationBbox(imagenbox, (0.3, 0.7), frameon=False)
-ax.add_artist(ab)
+    pokemonlist = [datos['name'].capitalize(), url_imagen ,peso, tamano, movimientos, habilidades, tipos] # crea una lista con la información del Pokémon
 
-# Agrega información del Pokémon al gráfico
-ax.text(0.6, 0.7, f"Nombre: {datos['name'].capitalize()}\n"
-                 f"Peso: {datos['weight']/10:.1f} kg\n"
-                 f"Tamaño: {datos['height']/10:.1f} m\n"
-                 f"Movimientos: {', '.join(movimientos)}\n"
-                 f"Habilidades: {', '.join(habilidades)}\n"
-                 f"Tipos: {', '.join(tipos)}",
-)
-# Desactivar los ejes del gráfico
-ax.axis('off')
-# Añadir un título al gráfico
-plt.title(f"Información de {datos['name'].capitalize()}", fontsize=16)
-plt.show()
+    s = pd.Series(pokemonlist, index=['Nombre', 'URLImagen', 'Peso', 'Tamaño', 'Movimientos', 'Habilidades', 'Tipos']) # crea una Serie de pandas con la información del Pokémon
 
+    df= pd.DataFrame([s]).to_json('pokedex/pokemon.json', index=False)# guarda la información del Pokémon en un archivo JSON
 
+    fg, ax = plt.subplots(figsize=(7, 5)) # crea una figura y un eje para el gráfico
+    imagenbox = OffsetImage(imagen, zoom=0.3) # crea un objeto OffsetImage con la imagen del Pokémon
+    ab = AnnotationBbox(imagenbox, (0.2, 0.5), frameon=False) # crea un objeto AnnotationBbox para colocar la imagen en el gráfico
+    ax.add_artist(ab) # añade la imagen al eje del gráfico
 
+    # Dibuja un rectángulo alrededor de la imagen
+    ax.text(0.6, 0.1, f"Nombre: {datos['name'].capitalize()}\n"
+                     f"Peso: {datos['weight']/10:.1f} kg\n"
+                     f"Tamaño: {datos['height']/10:.1f} m\n"
+                     f"Movimientos: {'\n '.join(movimientos)}\n"
+                     f"Habilidades: {', '.join(habilidades)}\n"
+                     f"Tipos: {', '.join(tipos)}",
+    )
+    # Desactivsa los ejes y muestra el título
+    ax.axis('off')
+    plt.title(f"Información de {datos['name'].capitalize()}", fontsize=20, fontweight='bold') # establece el título del gráfico
+
+    #muestra el gráfico
+    plt.show()
+
+# Verifica si el script se está ejecutando directamente
+
+if __name__ == "__main__":
+    pokemon = input("Ingresa el nombre del Pokémon: ") # Solicita al usuario que ingrese el nombre del Pokémon
+    pokemon = pokemon.lower().strip() # Convierte el nombre del Pokémon a minúsculas y elimina espacios en blanco al inicio y al final
+    if not pokemon:
+        print("No se ha ingresado un Pokémon. Intenta nuevamente.")
+    elif not pokemon.isalpha():
+        print("El nombre del Pokémon debe contener solo letras. Intenta nuevamente.")
+        exit()
+
+    obten_pokemon(pokemon)
+   
